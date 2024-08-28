66ea9e733928249f0bc6e2ad3d4c647cf1c52a11
Hossein Salari <hosein.salari81@gmail.com>
2024-08-28 15:14:24 +0330

    fetch category list

---
+++ src/app/blogs/_components/CategoryList.jsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+async function CategoryList() {
+  const res = await fetch("http://localhost:5000/api/category/list");
+  const {
+    data: { categories },
+  } = await res.json();
+
+  return (
+    <ul className="space-y-4">
+      <Link href="/blogs">همه</Link>
+      {categories.map((category) => (
+        <li key={category._id}>
+          <Link href={`/blogs/category/${category.slug}`}>
+            {category.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default CategoryList;
