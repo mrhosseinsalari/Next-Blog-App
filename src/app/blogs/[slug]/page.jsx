1d558dfa1c7b0eceff1cf44d68698d5f23c91a28
Hossein Salari <hosein.salari81@gmail.com>
2024-09-01 20:21:18 +0330

    generate static params in post detail page

--- src/app/blogs/[slug]/page.jsx
+++ src/app/blogs/[slug]/page.jsx
@@ -1,9 +1,17 @@
-import { getPostBySlug } from "@/services/postServices";
+import { getPostBySlug, getPosts } from "@/services/postServices";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
+export const dynamicParams = false;
+
+export async function generateStaticParams() {
+  const posts = await getPosts();
+  const slugs = posts.map((post) => ({ slug: post.slug }));
+  return slugs;
+}
+
 export async function generateMetadata({ params }) {
-  const post = await getPostBySlug(params.postSlug);
+  const post = await getPostBySlug(params.slug);
 
   return {
     title: `پست ${post.title}`,
@@ -13,7 +21,7 @@ export async function generateMetadata({ params }) {
 async function SinglePost({ params }) {
   //   await new Promise((res) => setTimeout(() => res(), 1000));
 
-  const post = await getPostBySlug(params.postSlug);
+  const post = await getPostBySlug(params.slug);
   if (!post) notFound();
 
   return (
--- src/services/postServices.js
+++ src/services/postServices.js
@@ -1,3 +1,11 @@
+export async function getPosts() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/post/list`);
+  const { data } = await res.json();
+  const { posts } = data || {};
+
+  return posts;
+}
+
 export async function getPostBySlug(slug) {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/post/slug/${slug}`
