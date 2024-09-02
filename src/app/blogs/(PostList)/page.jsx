eb118afa222d9895c8bafe727646f019b9008505
Hossein Salari <hosein.salari81@gmail.com>
2024-09-03 01:58:10 +0330

    minor changes

--- src/app/blogs/(PostList)/page.jsx
+++ src/app/blogs/(PostList)/page.jsx
@@ -2,19 +2,7 @@ import { Suspense } from "react";
 import PostList from "../_components/PostList";
 import Spinner from "@/ui/Spinner";
 
-//? how to revalidate :
-
-//* 1. time based
-
-export const revalidate = 3600; // for all request on this route
-// after 15s => re-build =>
-// 1. pass time interval +
-// 2. new incoming request to rebuild this page
-// updated data will be shown to the next user !! => ISR => incremental static re-generation
-
-// { next: { revalidate: 3600 } } => for one request
-
-// export const revalidate = 0; => opt out => dynamic !!!
+export const revalidate = 3600;
 
 async function BlogPage() {
   return (
