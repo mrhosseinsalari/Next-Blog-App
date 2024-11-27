52dd599454ec38dac61b0bbbb5bc7d664116ca12
Hossein Salari <hosein.salari81@gmail.com>
2024-11-27 23:21:58 +0330

    minor changes

--- src/app/blogs/_components/Author.jsx
+++ src/app/blogs/_components/Author.jsx
@@ -3,7 +3,7 @@ import Avatar from "@/ui/Avatar";
 function Author({ avatarUrl, name }) {
   return (
     <div className="flex items-center gap-x-1">
-      <Avatar src={avatarUrl} />
+      <Avatar src={avatarUrl} alt={name} />
       <span className="text-sm text-secondary-500">{name}</span>
     </div>
   );
