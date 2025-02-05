86d41bc4073122e49aa496de504c87e9ea6c7a2d
Hossein Salari <hosein.salari81@gmail.com>
2025-02-05 09:22:25 +0330

    create edit route for posts

---
+++ src/app/(dashboard)/profile/posts/[postId]/edit/page.jsx
@@ -0,0 +1,30 @@
+import { getPostById } from "@/services/postService";
+import Breadcrumbs from "@/ui/BreadCrumbs";
+import { notFound } from "next/navigation";
+import CreatePostForm from "../../create/_/CreatePostForm";
+
+async function EditPage({ params: { postId } }) {
+  const { post } = await getPostById(postId);
+  if (!post) notFound();
+
+  const breadcrumbs = [
+    {
+      label: "پست ها",
+      href: "/profile/posts",
+    },
+    {
+      label: "ویرایش پست",
+      href: `/profile/posts/${postId}/edit`,
+      active: true,
+    },
+  ];
+
+  return (
+    <div>
+      <Breadcrumbs breadcrumbs={breadcrumbs} />
+      <CreatePostForm />
+    </div>
+  );
+}
+
+export default EditPage;
